273fa3afc43f01962a860ea1fcb5a70027476867
jbifeld2 <jbifeld2@illinois.edu>
2021-05-06 17:21:21 -0500

    bootstrap addition / header update

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="header">
+    <b-navbar class="navbar" toggleable="lg" type="dark">
+      <b-navbar-brand class="name">Justin Bifeld</b-navbar-brand>
+      
+      <b-navbar-toggle target="nav-collapse" @click="grayOutMain"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" class="mt-2" is-nav>
+        <b-navbar-nav class="ml-auto mb-3">
+            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">About</b-button>
+            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Projects</b-button>
+            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Experience</b-button>
+            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Education</b-button>
+            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Recipes</b-button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  },
+  methods: {
+    grayOutMain(){
+      //document.getElementById("nav-collapse").classList.visible = true;
+      if(document.getElementById("Main-Body").classList.contains("grayed"))
+        document.getElementById("Main-Body").classList.remove("grayed")
+      else
+        document.getElementById("Main-Body").classList.add("grayed") 
+    },
+    scrollToSection(section){
+      console.log(section);
+      this.grayOutMain();
+      // Collapse Bar
+      // Scroll to section
+      // Call grayOutMain
+    }
+    // Event listener for when user expands window making navbar not longer collapsed (causes opacity changes  to remain, need to remove when no longer collapsable)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar{
+  background: black;
+  text-align: center;
+  width: 100%;
+  position: fixed;
+}
+.name{
+  font-weight: bold;
+  font-size: 30px;
+  left: 5%;
+
+}
+.button-list-item{
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-top: 10px;
+  background: black;
+}
+.button-list-item:hover{
+  font-weight: bold;
+}
+#nav-collapse{
+  @click
+}
+</style>
---
+++ src/components/MainBody.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Main-Body" id="Main-Body">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Main-Body{
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0deg, #4e94da 0%, #34004b 100%);
+  top: 50px;
+  position: absolute;
+}
+.grayed{
+  opacity: 0.5 !important;
+  filter: brightness(50%);
+}
+</style>
