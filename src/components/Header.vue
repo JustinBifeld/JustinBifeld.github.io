5416d7336cbed234805621f0a1e8326914e1cf03
jbifeld2 <jbifeld2@illinois.edu>
2021-05-07 19:02:50 -0500

    Working Navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,24 @@
     <b-navbar class="navbar" toggleable="lg" type="dark">
       <b-navbar-brand class="name">Justin Bifeld</b-navbar-brand>
       
-      <b-navbar-toggle target="nav-collapse" @click="grayOutMain"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" class="mt-2" is-nav>
         <b-navbar-nav class="ml-auto mb-3">
-            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">About</b-button>
-            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Projects</b-button>
-            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Experience</b-button>
-            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Education</b-button>
-            <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Recipes</b-button>
+            <template v-if="isCollapsed"> <!-- Collapsed menu is open (menu is down) , can be toggled to close menu (more for mobile)-->
+              <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection('About')">About</b-button>
+              <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection('Projects')">Projects</b-button>
+              <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Experience</b-button>
+              <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Education</b-button>
+              <b-button class="button-list-item" v-b-toggle.nav-collapse @click="scrollToSection">Recipes</b-button>
+            </template>
+            <template v-else> <!-- Collapsed menu is closed (menu is up), can't be toggled change menu (more for desltop) -->
+              <b-button class="button-list-item" @click="scrollToSection('About')">About</b-button>
+              <b-button class="button-list-item" @click="scrollToSection('Projects')">Projects</b-button>
+              <b-button class="button-list-item" @click="scrollToSection">Experience</b-button>
+              <b-button class="button-list-item" @click="scrollToSection">Education</b-button>
+              <b-button class="button-list-item" @click="scrollToSection">Recipes</b-button>
+            </template>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -24,22 +33,28 @@ export default {
   props: {
     msg: String
   },
+  data: () => ({
+    isCollapsed: false,
+    About: "About",
+  }),
   methods: {
-    grayOutMain(){
-      //document.getElementById("nav-collapse").classList.visible = true;
-      if(document.getElementById("Main-Body").classList.contains("grayed"))
-        document.getElementById("Main-Body").classList.remove("grayed")
-      else
-        document.getElementById("Main-Body").classList.add("grayed") 
-    },
-    scrollToSection(section){
-      console.log(section);
-      this.grayOutMain();
-      // Collapse Bar
+    scrollToSection: function(sectionText){
+      console.log(sectionText);
       // Scroll to section
-      // Call grayOutMain
-    }
+      document.getElementById(sectionText).scrollIntoView({
+        behavior: "smooth",
+      })
+    },
     // Event listener for when user expands window making navbar not longer collapsed (causes opacity changes  to remain, need to remove when no longer collapsable)
+  },
+  mounted() {
+      this.$root.$on('bv::collapse::state', (collapseId, isJustShown) => {
+        this.isCollapsed = isJustShown
+        if(isJustShown)
+          document.getElementById("Main-Body").classList.add("grayed")
+        else
+          document.getElementById("Main-Body").classList.remove("grayed")
+      })
   }
 }
 </script>
@@ -55,8 +70,9 @@ export default {
 .name{
   font-weight: bold;
   font-size: 30px;
+  margin-top: 10px;
+  margin-bottom: 10px;
   left: 5%;
-
 }
 .button-list-item{
   margin-left: 30px;
@@ -66,8 +82,5 @@ export default {
 }
 .button-list-item:hover{
   font-weight: bold;
-}
-#nav-collapse{
-  @click
 }
 </style>
--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="Main-Body" id="Main-Body">
-    
+    <section id="About">
+      <p class="About-Text">
+        About Text - Coming Soon!
+      </p>
+    </section>
+    <section id="Projects">
+      <p class="Projects-Text">
+        Projects Text - Coming Soon!
+      </p>
+    </section>
   </div>
 </template>
 
@@ -19,11 +28,27 @@ export default {
   width: 100%;
   height: 100%;
   background: linear-gradient(0deg, #4e94da 0%, #34004b 100%);
-  top: 50px;
+  top: 90px;
   position: absolute;
 }
 .grayed{
-  opacity: 0.5 !important;
+  /*Change brightness of everything except header, used when header collapsable menu is opened*/
   filter: brightness(50%);
+}
+#About{
+  background: rgb(13, 13, 60);
+  width: 100%;
+  height: calc(100vh - 90px);
+}
+.About-Text{
+  color: white;
+}
+#Projects{
+  width: 100%;
+  height: 500px;
+  background: rgb(116, 99, 5);
+}
+.Projects-Text{
+  color: white;
 }
 </style>
